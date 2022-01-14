c686cafb05d0163f95c353352bd2155fb7a403dd
Saverio <xavier.mitault@epitech.eu>
2022-01-14 12:37:43 +0100

    [remove] .ycm_global_ycm_extra_conf.py: no need

--- .ycm_global_ycm_extra_conf.py
+++
@@ -1,7 +0,0 @@
-def Settings( **kwargs ):
-    return {
-        'flags': [ '-x', 'c', '-Wall', '-Wextra', '-Werror',
-            '-Ilib/include', '-Iinclude', '-I../include', '-I../lib/include',
-            '-Ilib/includes', '-Iincludes', '-I../includes',
-            '-I../lib/includes'],
-    }
