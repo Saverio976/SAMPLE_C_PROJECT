9a6979f41a83646e6b28c05a65873d6d96be9d6b
saverio <xavier.mitault@epitech.eu>
2022-03-03 13:00:39 +0100

    Fix norm

--- lib/libwa/src/my_wordarray_show.c
+++ lib/libwa/src/my_wordarray_show.c
@@ -9,10 +9,9 @@
 #include "my_puts.h"
 
 /**
- * @brief print each char * of the 2d array with a newline after each char *
- *
- * @param array
- */
+** @brief print each char * of the 2d array with a newline after each char *
+** @param array
+**/
 void my_wordarray_showln(char *const *array)
 {
     if (array == NULL) {
@@ -25,10 +24,9 @@ void my_wordarray_showln(char *const *array)
 }
 
 /**
- * @brief print each char * of the 2d array
- *
- * @param array
- */
+** @brief print each char * of the 2d array
+** @param array
+**/
 void my_wordarray_show(char *const *array)
 {
     if (array == NULL) {
